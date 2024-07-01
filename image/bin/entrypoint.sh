755dc16b7e446c90341e188a14cf553c22ec2d74
Dmitry Smirnov <dmitry.smirnov@transactcampus.com>
2024-07-01 12:32:01 +0300

    additional entrypoint logic is enabled [UAT-32]

--- image/bin/entrypoint.sh
+++ image/bin/entrypoint.sh
@@ -7,4 +7,10 @@ if [ "$#" -gt 0 ]; then
 else
     # If no arguments are passed, execute the main.sh script
     exec /usr/local/bin/main.sh
+    
+    # If an additional entrypoint script exists in the child image, execute it
+    if [ -f $ADDITIONAL_ENTRYPOINT ]; then
+        echo "Executing additional entrypoint logic..."
+        $ADDITIONAL_ENTRYPOINT "$@"
+    fi
 fi
