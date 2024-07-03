55d5e7c7b89efc2be4999fcd11e6646a7af32d9c
Dmitry Smirnov <dmitry.smirnov@transactcampus.com>
2024-07-03 13:37:51 +0300

    improved azure_authenticate [UAT-31, UAT-16, UAT-13]

--- image/bin/modules/auth/azure.sh
+++ image/bin/modules/auth/azure.sh
@@ -3,29 +3,27 @@
 # Function to authenticate Azure accounts
 azure_authenticate() {
     local actor=$1
-    local type=$(echo "$actor" | yq e '.value.type' -)
-    local subscription=$(echo "$actor" | yq e '.value.subscription' -)
-    local tenant=$(echo "$actor" | yq e '.value.tenant' -)
-    local application=$(echo "$actor" | yq e '.value.application' -)
-    local password=$(echo "$actor" | yq e '.value.password' -)
-    local email=$(echo "$actor" | yq e '.value.email' -)
-    
+    local type=$(echo "$actor" | yq e '.type' -)
+    local subscription=$(echo "$actor" | yq e '.subscription' -)
+    local tenant=$(echo "$actor" | yq e '.tenant' -)
+    local application=$(echo "$actor" | yq e '.application' -)
+    local password=$(echo "$actor" | yq e '.password' -)
+    local email=$(echo "$actor" | yq e '.email' -)
+
     case $type in
         "azure-service-principal")
             echo "Authenticating Azure service principal: $application"
             az login --service-principal -u "$application" -p "$password" --tenant "$tenant"
             az account set --subscription "$subscription"
-        ;;
+            ;;
         "azure-personal-account")
             echo "Authenticating Azure personal account: $email"
             az login -u "$email" -p "$password"
-            if [ -n "$subscription" ]; then
-                az account set --subscription "$subscription"
-            fi
-        ;;
+            az account set --subscription "$subscription"
+            ;;
         *)
             echo "Error: Unsupported Azure authentication type $type"
             return 1
-        ;;
+            ;;
     esac
 }
