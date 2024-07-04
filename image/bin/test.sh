733ffea9f04d09706a0970cc42674836ed7680c9
Dmitry Smirnov <dmitry.smirnov@transactcampus.com>
2024-07-04 18:50:43 +0300

    fixed test [UAT-31]

--- image/bin/test.sh
+++ image/bin/test.sh
@@ -2,11 +2,14 @@
 
 echo "Starting tests..."
 
+# Debug: Print the value of DOCKER_IMAGE_NAME
+echo "DOCKER_IMAGE_NAME is set to: $DOCKER_IMAGE_NAME"
+
 # Test environment variables
-# if [ "$DOCKER_IMAGE_NAME" != "udx-worker" ]; then
-#     echo "DOCKER_IMAGE_NAME environment variable is not set correctly"
-#     exit 1
-# fi
+if [ "$DOCKER_IMAGE_NAME" != "udx-worker" ]; then
+    echo "DOCKER_IMAGE_NAME environment variable is not set correctly"
+    exit 1
+fi
 
 # Test secret resolution (assuming secrets are set as environment variables)
 if [ -z "$AZURE_SECRET" ]; then
@@ -19,5 +22,10 @@ az --version
 
 # Test Azure CLI login
 az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_APPLICATION_PASSWORD --tenant $AZURE_TENANT_ID
+
+if [ $? -ne 0 ]; then
+    echo "Azure CLI login failed"
+    exit 1
+fi
 
 echo "All tests passed successfully."
