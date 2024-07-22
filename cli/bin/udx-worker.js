d410b516cd73604f47e3dd84a392da71233f9429
Dmitry Smirnov <dmitry.smirnov@transactcampus.com>
2024-07-22 15:20:48 +0300

    improved cli [UAT-29]

--- cli/bin/udx-worker.js
+++ cli/bin/udx-worker.js
@@ -2,15 +2,29 @@
 
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-import { execFile, exec } from "child_process";
+import { exec } from "child_process";
 import { program } from 'commander';
+import readline from 'readline';
+import fs from 'fs';
+import yaml from 'yaml';
+import { readFileSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-function executeScript(scriptName, actionName) {
-  const script = join(__dirname, `${scriptName}.sh`);
-  execFile(script, (error, stdout, stderr) => {
+const configPath = '/src/configs/worker.yml';
+
+// Load the worker configuration
+const configFile = readFileSync(configPath, 'utf8');
+const config = yaml.parse(configFile);
+
+const questions = Object.keys(config.config.env).map(key => ({
+  question: `Enter ${key.replace(/_/g, ' ')}: `,
+  key
+}));
+
+function executeCommand(command, actionName) {
+  exec(command, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error ${actionName}:`, error);
       return;
@@ -20,27 +34,37 @@ function executeScript(scriptName, actionName) {
   });
 }
 
-function executeMake(target, actionName) {
-  exec(`make -f ${join(__dirname, 'Makefile')} ${target}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error ${actionName}:`, error);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+function executeMake(target, actionName, options = '') {
+  const cmd = `make -f ${join(__dirname, '../Makefile')} ${target} ${options}`;
+  executeCommand(cmd, actionName);
 }
 
-// Define CLI commands using commander
-program
-  .command('start')
-  .description('Start the worker')
-  .action(() => executeScript("start", "starting"));
+function generateEnvFile() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-program
-  .command('restart')
-  .description('Restart the worker')
-  .action(() => executeScript("restart", "restarting"));
+  let envContent = '';
+  let currentQuestionIndex = 0;
+
+  function askNextQuestion() {
+    if (currentQuestionIndex < questions.length) {
+      rl.question(questions[currentQuestionIndex].question, (answer) => {
+        envContent += `${questions[currentQuestionIndex].key}=${answer}\n`;
+        currentQuestionIndex++;
+        askNextQuestion();
+      });
+    } else {
+      rl.close();
+      const envFilePath = join(process.cwd(), '.udx');
+      fs.writeFileSync(envFilePath, envContent, 'utf8');
+      console.log('.udx environment file generated successfully.');
+    }
+  }
+
+  askNextQuestion();
+}
 
 program
   .command('build')
@@ -51,19 +75,19 @@ program
   .command('run')
   .description('Run the Docker container')
   .option('-e, --env-file <path>', 'Path to the environment file', './.udx')
-  .action((options) => executeMake(`run ENV_FILE=${options.envFile}`, 'running'));
+  .action((options) => executeMake('run', 'running', `ENV_FILE=${options.envFile}`));
 
 program
   .command('run-interactive')
   .description('Run the Docker container interactively')
   .option('-e, --env-file <path>', 'Path to the environment file', './.udx')
-  .action((options) => executeMake(`run-interactive ENV_FILE=${options.envFile}`, 'running interactively'));
+  .action((options) => executeMake('run-interactive', 'running interactively', `ENV_FILE=${options.envFile}`));
 
 program
   .command('exec')
   .description('Exec into the running Docker container')
   .option('-c, --container-name <name>', 'Container name', 'udx-worker-container')
-  .action((options) => executeMake(`exec CONTAINER_NAME=${options.containerName}`, 'executing'));
+  .action((options) => executeMake('exec', 'executing', `CONTAINER_NAME=${options.containerName}`));
 
 program
   .command('delete')
@@ -76,22 +100,20 @@ program
   .action(() => executeMake('log', 'viewing logs'));
 
 program
-  .command('trivy-basic')
-  .description('Perform basic Trivy scanning')
-  .option('-i, --docker-image <image>', 'Docker image to scan', 'your-docker-image:tag')
-  .action((options) => executeMake(`trivy-basic DOCKER_IMAGE=${options.dockerImage}`, 'Trivy scanning'));
-
-program
-  .command('trivy-severity')
-  .description('Perform Trivy scanning with specific severity')
-  .option('-i, --docker-image <image>', 'Docker image to scan', 'your-docker-image:tag')
-  .option('-s, --severity <severity>', 'Severity level', 'CRITICAL')
-  .action((options) => executeMake(`trivy-severity DOCKER_IMAGE=${options.dockerImage} TRIVY_SEVERITY=${options.severity}`, 'Trivy scanning'));
-
-program
   .command('gcr-login')
   .description('Log in to Google Cloud Artifact Registry')
   .action(() => executeMake('gcr-login', 'GCR login'));
+
+program
+  .command('generate-env')
+  .description('Generate the .udx environment file')
+  .action(() => generateEnvFile());
+
+program
+  .command('pull')
+  .description('Pull Docker image from repository')
+  .option('-i, --image <image>', 'Docker image to pull', 'udx-worker/udx-worker:latest')
+  .action((options) => executeMake('pull', 'pulling image', `DOCKER_IMAGE=${options.image}`));
 
 program
   .command('help')
