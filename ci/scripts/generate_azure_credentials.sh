3f51d23d132eb18488bf4f59128faf42c6fab439
Dmitry Smirnov <dmitry.smirnov@transactcampus.com>
2024-07-29 21:54:09 +0300

    fix

--- ci/scripts/generate_azure_credentials.sh
+++ ci/scripts/generate_azure_credentials.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Extract values from .udx file
-AZURE_SUBSCRIPTION_ID=$(grep AZURE_SUBSCRIPTION_ID .udx | cut -d '=' -f2 | tr -d '\r')
-AZURE_TENANT_ID=$(grep AZURE_TENANT_ID .udx | cut -d '=' -f2 | tr -d '\r')
-AZURE_APPLICATION_ID=$(grep AZURE_APPLICATION_ID .udx | cut -d '=' -f2 | tr -d '\r')
-AZURE_APPLICATION_PASSWORD=$(grep AZURE_APPLICATION_PASSWORD .udx | cut -d '=' -f2 | tr -d '\r')
+AZURE_SUBSCRIPTION_ID=$(grep AZURE_SUBSCRIPTION_ID .udx | cut -d '=' -f2)
+AZURE_TENANT_ID=$(grep AZURE_TENANT_ID .udx | cut -d '=' -f2)
+AZURE_APPLICATION_ID=$(grep AZURE_APPLICATION_ID .udx | cut -d '=' -f2)
+AZURE_APPLICATION_PASSWORD=$(grep AZURE_APPLICATION_PASSWORD .udx | cut -d '=' -f2)
 
-# Generate AZURE_CREDENTIALS in JSON format and export as environment variable
-AZURE_CREDENTIALS=$(jq -n --arg clientId "$AZURE_APPLICATION_ID" --arg clientSecret "$AZURE_APPLICATION_PASSWORD" --arg subscriptionId "$AZURE_SUBSCRIPTION_ID" --arg tenantId "$AZURE_TENANT_ID" \
-'{
-  clientId: $clientId,
-  clientSecret: $clientSecret,
-  subscriptionId: $subscriptionId,
-  tenantId: $tenantId,
-  activeDirectoryEndpointUrl: "https://login.microsoftonline.com",
-  resourceManagerEndpointUrl: "https://management.azure.com/",
-  activeDirectoryGraphResourceId: "https://graph.windows.net/",
-  sqlManagementEndpointUrl: "https://management.core.windows.net:8443/",
-  galleryEndpointUrl: "https://gallery.azure.com/",
-  managementEndpointUrl: "https://management.core.windows.net/"
-}')
-echo "AZURE_CREDENTIALS=$(echo $AZURE_CREDENTIALS | jq -c .)" >> $GITHUB_ENV
+# Create Azure credentials JSON file
+cat <<EOF > azure_credentials.json
+{
+  "clientId": "$AZURE_APPLICATION_ID",
+  "clientSecret": "$AZURE_APPLICATION_PASSWORD",
+  "subscriptionId": "$AZURE_SUBSCRIPTION_ID",
+  "tenantId": "$AZURE_TENANT_ID",
+  "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+  "resourceManagerEndpointUrl": "https://management.azure.com/",
+  "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+  "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+  "galleryEndpointUrl": "https://gallery.azure.com/",
+  "managementEndpointUrl": "https://management.core.windows.net/"
+}
+EOF
