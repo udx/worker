a8e130a00889df982271ea695b7c5df7f27fde81
Dmitry Smirnov <dmitry.smirnov@transactcampus.com>
2024-09-23 21:08:02 +0300

    cleanup

--- lib/auth/bitwarden.sh
+++ lib/auth/bitwarden.sh
@@ -4,20 +4,8 @@
 #
 # Example usage of the function
 # bitwarden_authenticate "/path/to/your/bitwarden_creds.json"
-#
-# Example Bitwarden credentials JSON file:
-#
-# {
-#     "clientId": "your-client-id",
-#     "clientSecret": "your-client-secret",
-#     "masterPassword": "your-master-password"
-# }
-#
 
 # Function to authenticate Bitwarden using API key or master password
 bitwarden_authenticate() {
     echo "Not supported yet. Is in progress"
 }
-
-# Example usage of the function
-# bitwarden_authenticate "/path/to/your/bitwarden_creds.json"
