3bcd8f16bf77095e28eeb28079c7a745cf33e2a4
Dmitry Smirnov <dmitry.smirnov@transactcampus.com>
2024-09-24 11:48:20 +0300

    high vulnerability test

--- lib/auth/gcp.sh
+++ lib/auth/gcp.sh
@@ -4,6 +4,22 @@
 #
 # Example usage of the function
 # gcp_authenticate "/path/to/your/gcp_creds.json"
+#
+# Example GCP credentials JSON file:
+#
+# {
+#     "type": "service_account",
+#     "project_id": "your-project-id",
+#     "private_key_id": "your-private-key-id",
+#     "private_key": "your-private-key",
+#     "client_email": "your-client-email",
+#     "client_id": "your-client-id",
+#     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+#     "token_uri": "https://oauth2.googleapis.com/token",
+#     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+#     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/your-client-email"
+# }
+#
 
 # Function to authenticate GCP service accounts
 gcp_authenticate() {
