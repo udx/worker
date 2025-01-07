6eded2713ce72d6d234d7db5dca5fdf3b5652545
Dmitry Smirnov <dmitry.smirnov@transactcampus.com>
2025-01-07 13:13:42 +0200

    cli readme

--- lib/cli.sh
+++ lib/cli.sh
@@ -16,12 +16,13 @@ check_status() {
 
 # Function to follow logs for a specific service
 follow_logs() {
-    tail -f /var/log/supervisor/"$1"-*.log
+    local type=${2:-out}  # Use 'out' if $2 is unset or null.
+    tail -f /var/log/supervisor/"$1"."$type".log
 }
 
 # Function to show supervisor configuration
 show_config() {
-    cat /etc/supervisor/supervisord.conf
+    cat /etc/supervisord.conf
 }
 
 # Function to start, stop, or restart a service
@@ -31,23 +32,41 @@ manage_service() {
 
 # CLI Interface
 case $1 in
-    list)
-        list_services
-        ;;
-    status)
-        check_status "$2"
-        ;;
-    logs)
-        follow_logs "$2"
-        ;;
-    config)
-        show_config
-        ;;
-    start|stop|restart)
-        manage_service "$1" "$2"
-        ;;
+    service)
+        shift # Shift the arguments to the left, so $2 becomes $1, $3 becomes $2, etc.
+        case $1 in
+            list)
+                list_services
+            ;;
+            status)
+                check_status "$2"
+            ;;
+            logs)
+                follow_logs "$2"
+            ;;
+            errors)
+                follow_logs "$2" "err"
+            ;;            
+            config)
+                show_config
+            ;;
+            start)
+                manage_service start "$2"
+            ;;
+            stop)
+                manage_service stop "$2"
+            ;;
+            restart)
+                manage_service restart "$2"
+            ;;
+            *)
+                echo "Usage: $0 service {list|status [service_name]|logs <service_name>|config|start <service_name>|stop <service_name>|restart <service_name>}"
+                exit 1
+            ;;
+        esac
+    ;;
     *)
-        echo "Usage: $0 {list|status [service_name]|logs <service_name>|config|start <service_name>|stop <service_name>|restart <service_name>}"
+        echo "Usage: $0 {service {list|status [service_name]|logs <service_name>|config|start <service_name>|stop <service_name>|restart <service_name>}}"
         exit 1
-        ;;
+    ;;
 esac
