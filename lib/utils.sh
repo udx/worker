db2120ad0c2047bbc078a4ae807ab020b7a81f7f
Dmitry Smirnov <dmitry.smirnov@transactcampus.com>
2025-07-04 17:27:28 +0300

    improved logs to be compatible with cloud providers

--- lib/utils.sh
+++ lib/utils.sh
@@ -8,24 +8,24 @@ resolve_env_vars() {
 
 log_info() {
     if [ $# -eq 1 ]; then
-        printf "ℹ️ %s\n" "$1" >&2
+        printf "\033[1;34m[INFO]\033[0m %s\n" "$1" >&2
     else
-        printf "ℹ️ %s: %s\n" "$1" "$2" >&2
+        printf "\033[1;34m[INFO]\033[0m %s: %s\n" "$1" "$2" >&2
     fi
 }
 
 log_warn() {
-    printf "⚠️ %s: %s\n" "$1" "$2" >&2
+    printf "\033[1;33m[WARN]\033[0m %s: %s\n" "$1" "$2" >&2
 }
 
 log_error() {
-    printf "❌ %s: %s\n" "$1" "$2" >&2
+    printf "\033[1;31m[ERROR]\033[0m %s: %s\n" "$1" "$2" >&2
 }
 
 log_success() {
-    printf "✅ %s: %s\n" "$1" "$2" >&2
+    printf "\033[1;32m[SUCCESS]\033[0m %s: %s\n" "$1" "$2" >&2
 }
 
 log_debug() {
-    printf "%s: %s\n" "$1" "$2" >&2
+    printf "\033[1;35m[DEBUG]\033[0m %s: %s\n" "$1" "$2" >&2
 }
